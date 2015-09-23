bc98474f965cd97589c56f103a19ff901d677d71
Ronni Hartung <ronn2807@easv365.dk>
2015-09-23 09:54:04 +0200

    Layered

---
+++ MusicStoreDAL/Album.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MusicStoreDAL
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string AlbumArtUrl { get; set; }
+        public string SongSampleUrl { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        // Foreign key properties.
+        // These properties are not strictly necessary, if the class has
+        // navigation propperties, but they have their advantages. If they
+        // are not present, the scaffolded AlbumsController and it's views
+        // will have no knowledge about an associated Artist and Genre.
+        public int ArtistId { get; set; }
+        public int GenreId { get; set; }
+
+        // The navigation properties below must be declared as virtual because
+        // EF must override them in order to make the navigation work.
+        public virtual Artist Artist { get; set; }
+        public virtual Genre Genre { get; set; }
+    }
+}
---
+++ MusicStoreDAL/Artist.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace MusicStoreDAL
+{
+    public class Artist
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        // The navigation property below must be declared as virtual because
+        // EF must override it in order to make the navigation work.
+        public virtual List<Album> Albums { get; set; }
+    }
+}
